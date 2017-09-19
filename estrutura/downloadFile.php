fb62853467e528d9f824fafbbff640470b134ca2
kelvinott <kelvinott3112@gmail.com>
2017-09-18 23:12:07 -0300

    download testa ai

--- estrutura/downloadFile.php
+++ estrutura/downloadFile.php
@@ -1,6 +1,26 @@
 <?php
 
-echo $_GET["nmRelatorio"];
+  $file =  "../phpjasper/geekcom/phpjasper/examples/" .  $_GET["nmRelatorio"];
+
+  if (file_exists($file)) {
+    $fp = fopen($file,"r");
+    $strPDF = fread($fp,filesize($file));
+    fclose($fp);
+
+    header('Content-Type: application/x-download');
+    header("Content-disposition: attachment; filename=\"". $_GET["nmRelatorio"] . "\"");
+    header("Expires: 0");
+    header("Cache-Control: no-cache");
+    header('Cache-Control: private, max-age=0, must-revalidate');
+    header("Pragma: public");
+
+    echo $strPDF;
+
+  } else {
+    //mensagem que arquivo não existe
+  }
+
+
 
 
 ?>
